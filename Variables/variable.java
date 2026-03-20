f7346aae1ee438e503a41de3e9d9ec0c4d22d9b2
Pragana Shrestha <shresthapragana@gmail.com>
2026-03-20 19:14:32 +0545

    Commit and Push

---
+++ Variables/variable.java
@@ -0,0 +1,14 @@
+package Variables;
+
+public class variable {
+    public static void main(String[] args) {
+        int age = 25; // This is a variable declaration and initialization
+        String name = "John"; // Another variable declaration and initialization
+
+        double salary = 50000.50; // A variable to store salary
+
+        System.out.println("Age: " + age);
+        System.out.println("Name: " + name);
+        System.out.println("Salary: " + salary);
+    }
+}
