2e64211bba4ad4c10297e8e5219da86302808f65
Pragana Shrestha <shresthapragana@gmail.com>
2026-03-25 20:10:50 +0545

    Commit and Push

---
+++ Variables/identifiers.java
@@ -0,0 +1,6 @@
+public class identifiers {
+    public static void main(String[] args) {
+        int MinutesInHour = 60;
+        System.out.println("Minutes in an hour:" + MinutesInHour);
+    }
+}
