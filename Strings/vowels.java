0e71e1d48e87ae98cabba725e109d96a993b85f2
Pragana Shrestha <shresthapragana@gmail.com>
2026-03-27 21:59:50 +0545

    Commit and Push

--- Strings/vowels.java
+++ Strings/vowels.java
@@ -1,10 +1,12 @@
 public class vowels {
-    String str = "Pragana";
-    int count = 0;
-    for(char c: str.toLowerCase().toCharArray()){
-        if ("aeiou".indexOf(c) != -1){
-            count++;
-        }
+    public static void main(String[] args) {
+         String str = "Pragana";
+   int count = 0;
+for(char c : str.toLowerCase().toCharArray()) {
+    if("aeiou".indexOf(c) != -1) 
+        count++;
+}
+System.out.println("Vowels: " + count); 
     }
-    System.out.println("Vowels:" + count);
+  
 }
